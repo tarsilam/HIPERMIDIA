5332ded0a006368a2c1d8126243bce0736cb1d6e
tarsilam <38993342+tarsilam@users.noreply.github.com>
2021-07-19 12:08:13 -0300

    Add files via upload

---
+++ PAR-ÍMPAR/par_impar.js
@@ -0,0 +1,19 @@
+valor = prompt("entre com numero")
+
+  var resultado = par()
+
+ function par(){
+  
+  var teste = valor % 2;
+
+  
+  if( teste  == 0)
+    
+      alert('par')
+    
+  
+  else
+    
+      alert('impar')
+    
+}
